63bd22c32ec49e5ca19a320d45b18da60e0f819a
Anil Rayamajhi <anilrm07@gmail.com>
2016-09-09 12:23:50 -0700

    initial commit

---
+++ lab.rb
@@ -0,0 +1,24 @@
+my_file = File.open('lab_frequencies.txt', 'r')
+final_file = my_file.read
+
+words = final_file.split(" ")
+# p words
+
+frequencies = Hash.new(0)
+
+words.each { |el| frequencies[el] += 1 }
+
+sorted = frequencies.sort_by {|a, b| b}.reverse!
+
+# p sorted
+
+File.open('bananas.html', 'w') do |el|
+
+  el.puts "<table>"
+
+  sorted.each do |f|
+    el.puts "<tr><td><b>#{f[0]}</b></td><td><b>#{f[1]}</b></td></tr>"
+  end
+
+  el.puts "</table>"
+end
